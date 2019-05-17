d7cd517ba14eb359b69581dbcf87895d1d4ac23b
Jonas Enge <jonas.enge@t-fk.no>
2019-05-17 12:23:28 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = {
-  SYSTEM_NAME: process.env.SYSTEM_NAME || 'robot-stats',
+  SYSTEM_NAME: process.env.SYSTEM_NAME || '',
   DIRECTORIES_PATH: process.env.DIRECTORIES_PATH || 'test/directories',
   JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   STATS_SERVICE_URL: process.env.STATS_SERVICE_URL || 'https://stats.service.t-fk.no/stats',
